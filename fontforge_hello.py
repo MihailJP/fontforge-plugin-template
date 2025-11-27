742854252c4dcb1d9749832df1f1156a0e81b52c
MihailJP <mihailjp@gmail.com>
2025-11-27 22:31:01 +0900

    Fix style

--- fontforge_hello.py
+++ fontforge_hello.py
@@ -3,13 +3,15 @@ import fontforge
 def hello(u, glyph):
     fontforge.postNotice("FontForge Plugin Template", "Hello, world!")
 
+
 def helloEnable(u, glyph):
     return True
 
+
 def fontforge_plugin_init(**kw):
     fontforge.registerMenuItem(
-        callback = hello,
-        enable = helloEnable,
-        context = ("Font", "Glyph"),
-        name = "Hello"
+        callback=hello,
+        enable=helloEnable,
+        context=("Font", "Glyph"),
+        name="Hello"
     )
